d9ddfc627e2a3d9193686bc17b585bdb5c138e2a
Angel T <angel.t069@outlook.com>
2024-03-11 15:05:15 -0600

    Valida el rol del usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { authGuard } from './auth-guard.guard';
 
 export const routes: Routes = [
     { path: 'login', loadComponent:()=>import('./login/login.component').then(m=>m.LoginComponent)},
+    { path: 'home', loadComponent:()=>import('./home/home.component').then(m=>m.HomeComponent), canActivate: [authGuard]},
     { path: 'register', loadComponent:()=>import('./register-user/register-user.component').then(m=>m.RegisterUserComponent)},
     { path: 'paises' , loadComponent:()=>import('./empleados/empleados.component').then(m=>m.EmpleadosComponent), canActivate: [authGuard]},
 
