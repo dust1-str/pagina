86910734602a9bcb95dbb1e569368cf7ba5d1553
Alejandro <22170166@uttcampus.edu.mx>
2024-03-12 15:26:14 -0600

    ay wey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,21 @@ import { Component,Input  } from '@angular/core';
 import { DashboardComponent } from '././dashboard/dashboard.component';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule,DashboardComponent],
+  imports: [RouterOutlet, ReactiveFormsModule,DashboardComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pagina-practica';
   authMessage: string = '';
+  authStatus: boolean = false;
 
   constructor(private router: Router) { }
 
@@ -21,10 +24,15 @@ export class AppComponent {
     const rol = localStorage.getItem('role_id');
     if (rol === '3') {
       this.authMessage = 'Usuario no autenticado';
+      this.authStatus = false;
     } else {
       this.authMessage = 'Usuario autenticado';
+      this.authStatus = true;
     }
+    console.log(this.authStatus);
+    console.log(this.authMessage);
   }
+
   navigateTo(table: string): void {
     this.router.navigate([table.toLowerCase()]);
   }
