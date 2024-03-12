58089742393357e4640ac6d811d7084e288775bb
Alejandro <22170166@uttcampus.edu.mx>
2024-03-12 07:39:26 -0600

    Ya jala el router outleeeeeeeeeet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component,Input  } from '@angular/core';
+import { DashboardComponent } from '././dashboard/dashboard.component';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule,DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pagina-practica';
+  authMessage: string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    const rol = localStorage.getItem('role_id');
+    if (rol === '3') {
+      this.authMessage = 'Usuario no autenticado';
+    } else {
+      this.authMessage = 'Usuario autenticado';
+    }
+  }
+  navigateTo(table: string): void {
+    this.router.navigate([table.toLowerCase()]);
+  }
+  
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,13 @@
-import { Component,Input  } from '@angular/core';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DashboardComponent,RouterOutlet],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  authMessage: string = '';
 
-  constructor(private router: Router) { }
- 
-  ngOnInit() {
-    const rol = localStorage.getItem('role_id');
-    if (rol === '3') {
-      this.authMessage = 'Usuario no autenticado';
-    } else {
-      this.authMessage = 'Usuario autenticado';
-    }
-  }
-  navigateTo(table: string): void {
-    this.router.navigate([table.toLowerCase()]);
-  }
-  
-
-
-  logout(): void {
-    // Agrega la lógica de logout aquí
-  }
 }
