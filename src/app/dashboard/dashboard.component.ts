7b0903410471a8afdb827685b78a7c216ed9a771
Alejandro <22170166@uttcampus.edu.mx>
2024-03-13 17:06:52 -0600

    X

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,24 @@
 import { Component,Input  } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 
 
 export class DashboardComponent {
+  rol_user: string = "3";
 
   constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.rol_user = localStorage.getItem('role_id') || this.rol_user;
+  }
 
   @Input() authMessage: string = '';
   
