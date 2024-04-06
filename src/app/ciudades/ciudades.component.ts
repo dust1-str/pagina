4fd26da9934308300ed1ba8f9dff8f1ae55d1969
Angel T <angel.t069@outlook.com>
2024-04-05 20:08:57 -0600

    WebSocket hecho

--- src/app/ciudades/ciudades.component.ts
+++ src/app/ciudades/ciudades.component.ts
@@ -4,6 +4,7 @@ import { Objeto } from '../Core/Interfaces/objeto';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { TableComponent } from '../table/table.component';
 import { CommonModule } from '@angular/common';
+import pusherJs from 'pusher-js';
 
 @Component({
   selector: 'app-ciudades',
@@ -22,19 +23,44 @@ export class CiudadesComponent implements OnInit {
   backRoute: string = '/ciudades';
   rol_user: string = "3";
   catalogo: boolean = true;
+  pusher: any;
 
   constructor(private ciudadesService: CiudadesService) { }
-
   ngOnInit(): void {
     this.obtenerDatos();
     this.rol_user = localStorage.getItem('role_id') || this.rol_user;
-
+    this.iniciarPusher();
+  }
+  
+  ngOnDestroy(): void {
+    if (this.pusher) {
+      this.pusher.disconnect();
+    }
   }
 
-  actualizarElementos() {
-    this.ngOnInit();
+  actualizarElementos() 
+  { 
+    this.ngOnInit(); 
   }
-
+  
+  iniciarPusher() {
+    this.pusher = new pusherJs('15b9609bf288fff026dd', {
+      cluster: 'mt1',
+      forceTLS: false,
+      wsHost: window.location.hostname,
+      wsPort: 6001,
+      enabledTransports: ['ws']
+    });
+    let channel = this.pusher.subscribe('channel-ciudad');
+    channel.bind('App\\Events\\nuevaCiudad', (data: any) => {
+      console.log('Evento recibido:', data);
+      this.obtenerDatos();
+    });
+    this.pusher.connection.bind('connected', () => {
+      console.log('Conexión establecida');
+    });
+  }
+  
   obtenerDatos() {
     this.ciudadesService.obtenerElemento().subscribe(
       data => {
